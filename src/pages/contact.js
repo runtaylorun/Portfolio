c65266ef504bb588dfaa5ee59ae7d989ae88d0d0
Taylor Mills <taylormills190@gmail.com>
2021-09-25 20:58:03 -0500

    contact page start

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,8 +6,30 @@ const ContactPage = () => {
     return (
         <>
             <NavBar />
-            <main>
-
+            <main style={{ display: 'flex', justifyContent: 'space-evenly', height: '100vh', backgroundColor: 'hsl(0, 0%, 98%)' }}>
+                <section style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '50%', }}>
+                    <form style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', height: '75%' }}>
+                        <div className={classes.inputContainer}>
+                            <input style={{ width: '35%' }} className={classes.textBox} placeholder='First Name' type="text" />
+                            <input style={{ width: '35%' }} className={classes.textBox} placeholder='Last Name' type="text" />
+                        </div>
+                        <div className={classes.inputContainer}>
+                            <input className={classes.textBox} placeholder='Email' type="text" />
+                        </div>
+                        <div className={classes.inputContainer}>
+                            <textarea className={classes.textArea} placeholder='Leave a message...' type="text" />
+                        </div>
+                        <div className={classes.formButtons}>
+                            <button>Submit</button>
+                            <button type='reset'>Clear</button>
+                        </div>
+                    </form>
+                </section>
+                <aside style={{ width: '50%', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
+                    <p>Github: runtalyorundldldld....</p>
+                    <p>LinkedIn: ruintalykafd;al;d....</p>
+                    <p>Instagram: taylormills1903</p>
+                </aside>
             </main>
         </>
     )
