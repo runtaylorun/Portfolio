49245532689b93966285e8c36c7d14dc82384b6a
Taylor Mills <taylormills190@gmail.com>
2021-09-25 20:57:55 -0500

    Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,20 @@
 import * as React from "react"
+import logo from '../images/7218.jpg'
 import NavBar from '../components/NavBar'
+import IconBar from '../components/IconBar'
+import * as classes from '../css/index.module.css'
 
 const IndexPage = () => {
   return (
     <>
       <NavBar />
-      <main>
+      <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', height: '90vh' }}>
+        <img src={logo} className={classes.roundedImage} alt='self-portrait' />
+        <div>
+          <h2 style={{ fontSize: '48px', marginBottom: 0, textAlign: 'center' }}>Hey There!</h2>
+          <p>My name is Taylor and welcome to my portfolio.</p>
+        </div>
+        <IconBar />
       </main>
     </>
   )
